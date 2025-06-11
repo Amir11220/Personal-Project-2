2eb99c2e33e94cdb78f561cdd3382d819c67b86f
Amir11220 <amiryaman3@gmail.com>
2025-06-11 15:56:04 +0300

    Create mockData.jsx

---
+++ src/assets/mockData.jsx
@@ -0,0 +1,245 @@
+import Image1 from './images/product1.jpg';
+import Image2 from './images/product2.jpg';
+import Image3 from './images/product3.jpg';
+import Image4 from './images/product4.jpg';
+import Image5 from './images/product5.jpg';
+import Image6 from './images/product6.jpg';
+import Image7 from './images/product7.jpg';
+import Image8 from './images/product8.jpg';
+import Image9 from './images/product9.jpg';
+import Image10 from './images/product10.jpg';
+import Image11 from './images/images/beard_oil2.jpg';
+import Image12 from './images/images/black_set.jpg';
+import Image13 from './images/images/black_set2.jpg';
+import Image14 from './images/images/chanel.jpg';
+import Image15 from './images/images/cream.jpg';
+import Image16 from './images/images/jewerly.jpg';
+import Image17 from './images/images/lens.jpg';
+import Image18 from './images/images/lipstick.jpg';
+import Image19 from './images/images/noir_chanel.jpg';
+import Image20 from './images/images/polaroid.jpg';
+import Image21 from './images/images/shaker.jpg';
+import Image22 from './images/images/shoes.jpg';
+import Image23 from './images/images/shoes2.jpg';
+import Image24 from './images/images/shoes3.jpg';
+import Image25 from './images/images/shoes4.jpg';
+import Image26 from './images/images/smart_watch.jpg';
+import Image27 from './images/images/vance.jpg';
+import Image28 from './images/images/washbasin.jpg';
+import Image29 from './images/images/zenit_camera.jpg';
+
+export const Categories = [
+    "Electronics",
+    "Fashion",
+    "Home & Kitchen",
+    "Beauty",
+    "Sports",
+    "Automotive",
+];
+
+export const mockData = [
+    {
+        id: 1,
+        image: Image1,
+        name: "Patek Philippe",
+        title: "Patek Philippe",
+        price: 19.99,   
+    },
+    {
+        id: 2,
+        image: Image2,
+        name: "Beard Oil",
+        title: "Beard Oil",
+        price: 29.99,   
+    },
+    {
+        id: 3,
+        image: Image3,
+        name: "Nike Mark 3",
+        title: "Nike Mark 3",
+        price: 39.99,   
+    },
+    {
+        id: 4,
+        image: Image4,
+        name: "Cosmetics 101",
+        title: "Cosmetics 101",
+        price: 49.99,   
+    },
+    {
+        id: 5,
+        image: Image5,
+        name: "Apple Warranty",
+        title: "Apple Warranty",
+        price: 59.99,   
+    },
+    {
+        id: 6,
+        image: Image6,
+        name: "Shoes",
+        title: "Shoes",
+        price: 69.99,   
+    },
+    {
+        id: 7,
+        image: Image7,
+        name: "Headphones",
+        title: "Headphones",
+        price: 79.99,   
+    },
+    {
+        id: 8,
+        image: Image8,
+        name: "Accessories",
+        title: "Accessories",
+        price: 89.99,   
+    },
+    {
+        id: 9,
+        image: Image9,
+        name: "Creams",
+        title: "Creams",
+        price: 99.99,   
+    },
+    {
+        id: 10,
+        image: Image10,
+        name: "Beauty Set",
+        title: "Beauty Set",
+        price: 109.99,
+    },
+    {
+        id: 11,
+        image: Image11,
+        name: "Beard Oil",
+        title: "Beard Oil",
+        price: 30,
+    },
+    {
+        id: 12,
+        image: Image12,
+        name: "Black Set",
+        title: "Black Set",
+        price: 60,
+    },
+    {
+        id: 13,
+        image: Image13,
+        name: "Black Set",
+        title: "Black Set",
+        price: 54,
+    },
+    {
+        id: 14,
+        image: Image14,
+        name: "Chanel",
+        title: "Chanel",
+        price: 250,
+    },
+    {
+        id: 15,
+        image: Image15,
+        name: "Cream",
+        title: "Cream",
+        price: 15,
+    },
+    {
+        id: 16,
+        image: Image16,
+        name: "Jewerly",
+        title: "Jewerly",
+        price: 160,
+    },
+    {
+        id: 17,
+        image: Image17,
+        name: "Lens",
+        title: "Lens",
+        price: 99.99,
+    },
+    {
+        id: 18,
+        image: Image18,
+        name: "Lipsticl",
+        title: "Lipstick",
+        price: 10,
+    },
+    {
+        id: 19,
+        image: Image19,
+        name: "Noir Chanel",
+        title: "Noir Chanel",
+        price: 270,
+    },
+    {
+        id: 20,
+        image: Image20,
+        name: "Polaroid",
+        title: "Polaroid",
+        price: 120,
+    },
+    {
+        id: 21,
+        image: Image21,
+        name: "Shaker",
+        title: "Shaker",
+        price: 6,
+    },
+    {
+        id: 22,
+        image: Image22,
+        name: "Shoes",
+        title: "Shoes",
+        price: 35,
+    },
+    {
+        id: 23,
+        image: Image23,
+        name: "Shoes",
+        title: "Shoes",
+        price: 25,
+    },
+    {
+        id: 24,
+        image: Image24,
+        name: "Shoes",
+        title: "Shoes",
+        price: 40,
+    },
+    {
+        id: 25,
+        image: Image25,
+        name: "Shoes",
+        title: "Shoes",
+        price: 45,
+    },
+    {
+        id: 26,
+        image: Image26,
+        name: "Smart Watch",
+        title: "Smart Watch",
+        price: 160,
+    },
+    {
+        id: 27,
+        image: Image27,
+        name: "Vance",
+        title: "Vance",
+        price: 70,
+    },
+    {
+        id: 28,
+        image: Image28,
+        name: "Washbasin",
+        title: "Washbasin",
+        price: 42,
+    },
+    {
+        id: 29,
+        image: Image29,
+        name: "Zenit Camera",
+        title: "Zenit Camera",
+        price: 400,
+    },
+    
+]
